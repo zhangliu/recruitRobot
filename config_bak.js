2741a708d148c8a1c3b9fe6466fae19a3bdef3ed
yunnian.zhang <yunnian.zhang@yintech.cn>
2018-07-02 21:38:39 +0800

    modify

--- config_bak.js
+++ config_bak.js
@@ -1,25 +1,28 @@
+const Base64 = require('js-base64').Base64
+
 module.exports = {
-  username: 'xxxxxxxxxxxxx',
-  password: 'xxxxxxxxxxxxx',
+  username: '',
+  password: Base64.decode(''),
   nightmare: {
     waitTimeout: 60000,
   },
   run: {
-    times: 5,
+    times: 3,
     timeout: 5 * 60 * 1000,
   },
   email: {
     sender: {
-      host: 'smtp.exmail.qq.com',
+      // host: 'smtp.exmail.qq.com',
+      host: 'smtp.yeah.net',
       port: 465,
       secure: true, // use SSL
       auth: {
-        user: 'xxxxxxxxxxx',
-        pass: 'xxxxxxxxxxxxxxx',
+        user: '',
+        pass: '',
       },
     },
     receivers: [
-      'xxxxxxxxxxxxxxxx',
+      '',
     ],
   },
 }
